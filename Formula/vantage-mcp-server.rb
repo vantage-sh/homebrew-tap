92e8869b21df559670bc1e6d0b8960d145f40b8b
Nelson Crespo <nelson@vantage.sh>
2025-04-24 15:44:40 -0400

    chore: Update vantage-mcp-server to v0.0.2.

--- Formula/vantage-mcp-server.rb
+++ Formula/vantage-mcp-server.rb
@@ -1,8 +1,8 @@
 class VantageMcpServer < Formula
   desc "MCP Server for accessing the https://vantage.sh API for costs and usage data"
   homepage "https://github.com/vantage-sh/vantage-mcp-server"
-  url "https://github.com/vantage-sh/vantage-mcp-server/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "6dd868df752cce93cf8abf2b22f3ef01eb482031500955d8baa0dfd9fd139907"
+  url "https://github.com/vantage-sh/vantage-mcp-server/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "a7e7cab3afcd06e5b842c43b0953ca93b5d309d8c1df8c2020fdaacbbce4d0be"
   license "MIT"
 
   depends_on "go" => :build
