4977182d329bf64e4221206b7d2ce9c7584015d0
Nick Funnell <nick@vantage.sh>
2025-05-01 12:02:39 -0700

    chore: Bump Version.

--- Formula/vantage-mcp-server.rb
+++ Formula/vantage-mcp-server.rb
@@ -1,8 +1,8 @@
 class VantageMcpServer < Formula
   desc "MCP Server for accessing the https://vantage.sh API for costs and usage data"
   homepage "https://github.com/vantage-sh/vantage-mcp-server"
-  url "https://github.com/vantage-sh/vantage-mcp-server/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "a7e7cab3afcd06e5b842c43b0953ca93b5d309d8c1df8c2020fdaacbbce4d0be"
+  url "https://github.com/vantage-sh/vantage-mcp-server/archive/refs/tags/v0.0.4.tar.gz"
+  sha256 "8e7047864370a6a82b2e14c892a156b67fc070e79ca9d686898477bf3484467c"
   license "MIT"
 
   depends_on "go" => :build
